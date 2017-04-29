19046f72a95411e7f6dddeae13553fc985abdb79
Unknown <saahilparikh2000@gmail.com>
2017-04-29 16:07:26 -0700

    More

---
+++ functions.h
@@ -0,0 +1,29 @@
+//Functions
+
+setLeftDrive(int speed)
+{
+	#warning "motor[leftSide] = speed; has not been set"
+}
+
+setRightDrive(int speed)
+{
+	#warning "motor[RightSide] = speed; has not been set"
+}
+
+setLiftSpeed(int speed)
+{
+	#warning "motor[LiftMotors] = speed; has not been set"
+}
+
+setDrive(int rightSpeed, int leftSpeed)
+{
+	setRightDrive(rightSpeed);
+	setLeftDrive(leftSpeed);
+}
+
+drivePID(int target)
+{
+
+
+
+}
